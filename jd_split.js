d11458f497e932cc8e030066007cccfd0ec2830b
sundanaizhu <56495109+sundanaizhu@users.noreply.github.com>
2020-12-07 12:19:51 +0800

    Update jd_split.js

--- jd_split.js
+++ jd_split.js
@@ -37,7 +37,7 @@ if ($.isNode()) {
   cookiesArr.push(...[$.getdata('CookieJD'), $.getdata('CookieJD2')]);
 }
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
-$.newShareCodes = ['P04z54XCjVUnIaW5jcJC2Hx3nhIiKwfDVW7Bg', 'P04z54XCjVUnIaW5nZNVjSpgy0T3_foc-1QAg', 'P04z54XCjVUnIaW5u2ak7ZCdan1BT0NlbBGZ1-rnMYj'];
+$.newShareCodes = ['P04z54XCjVUnIaW5jcJC2Hx3nhIiKwfDVW7Bg', 'P04z54XCjVUnIaW5nZNVjSpgy0T3_foc-1QAg', 'P04z54XCjVUnIaW5mZWVTqhgysZzfY_'];
 !(async () => {
   if (!cookiesArr[0]) {
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/', {"open-url": "https://bean.m.jd.com/"});
